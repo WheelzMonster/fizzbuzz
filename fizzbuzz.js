9dbf9538c58aab9d8b82ca680e4980f93f8d68c0
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2018-05-17 11:20:17 +0200

    correction
    corrections apportées à la valeur 0

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,13 @@
 //
 
-for(i = 0; i<=15; i++){
-    if(i !== 0){
+for (i = 0; i <= 15; i++) {
+    if (i === 0) {
         console.log(0);
-    } else if((i % 3 === 0) && (i % 5 === 0)){
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
         console.log("fizzbuzz");
-    } else if (i % 3 === 0){
+    } else if (i % 3 === 0) {
         console.log("fizz");
-    } else if (i % 5 === 0){
+    } else if (i % 5 === 0) {
         console.log("buzz");
     } else {
         console.log(i);
